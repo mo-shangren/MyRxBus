1a5107a384cefa008fafa3dc9cf31aeb4912941d
kill <935270110@qq.com>
2018-12-13 14:02:15 +0800

    　修改

--- app/src/main/java/com/self/rxdemo/Test2Activity.java
+++ app/src/main/java/com/self/rxdemo/Test2Activity.java
@@ -1,22 +1,26 @@
 package com.self.rxdemo;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.jakewharton.rxbinding2.widget.RxTextView;
 import com.killr.rxbus.RxBus;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class Test2Activity extends AppCompatActivity implements View.OnClickListener {
 
     TextView btn1, btn2, btn3, btn4;
     TestProduce mProduce = new TestProduce();
 
+    @SuppressLint("CheckResult")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,6 +37,11 @@ public class Test2Activity extends AppCompatActivity implements View.OnClickList
         btn4.setOnClickListener(this);
 
         RxBus.get().register(this);
+
+        //跟rxbinding并不冲突
+        RxTextView.textChanges(btn4)
+                .debounce(100, TimeUnit.MILLISECONDS)
+                .map(str -> str.toString().length() >= 6 && str.toString().length() <= 11);
 
     }
 
